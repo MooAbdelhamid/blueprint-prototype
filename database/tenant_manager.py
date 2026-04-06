26a45578a41c2ccc659ded2c78db82e44b9e610f
MooAbdelhamid <moethedataanalyst@gmail.com>
2026-04-06 23:24:59 +0200

    Created central databases & Working on the tables

--- database/tenant_manager.py
+++ database/tenant_manager.py
@@ -5,7 +5,6 @@ Construction
 import psycopg2
 from config import DatabaseConfig
 from psycopg2 import sql
-from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 
 class TenantDatabaseManager:
@@ -22,27 +21,34 @@ class TenantDatabaseManager:
 
         self._ensure_central_database()
 
-        # self._init_central_database()
+        self._init_central_database()
 
         print("Tenant database initialized")
 
     def _ensure_central_database(self):
         """
         Create the central database if it doesnt exist
+        Steps:
+        1. Connect to database
+        2. Check if exists
+        3. Create if missing
         """
+        # Connect to server
         conn_params = self.config.get_connection_string("postgres")
         conn = psycopg2.connect(**conn_params)
 
-        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        # Autocommit allow database creation
+        conn.autocommit = True
 
         cursor = conn.cursor()
 
         try:
+            # Check if database exists
             cursor.execute(
                 """
                 SELECT 1 FROM pg_database
                 WHERE datname = %s
-            """,
+                """,
                 (self.config.CENTRAL_DB,),
             )
 
@@ -66,3 +72,25 @@ class TenantDatabaseManager:
         finally:
             cursor.close()
             conn.close()
+
+    def _init_central_database(self):
+        """
+        Create tables in central database
+        """
+        conn = self._get_central_connection()
+        cursor = conn.cursor()
+
+        try:
+            pass
+        except Exception:
+            pass
+        finally:
+            cursor.close()
+            conn.close()
+
+    def _get_central_connection(self):
+        """
+        Get a connection to the central database
+        """
+        conn_params = self.config.get_connection_string(self.config.CENTRAL_DB)
+        return psycopg2.connect(**conn_params)
