2ef654816fad0d020be81ec87822ceb08e72e8c9
MooAbdelhamid <moethedataanalyst@gmail.com>
2026-04-17 20:35:55 +0200

    Added unique to company_name in tenats table

--- database/schemas/tenants.py
+++ database/schemas/tenants.py
@@ -2,7 +2,7 @@ def create_tenants_table(cursor):
     cursor.execute("""
                 CREATE TABLE IF NOT EXISTS tenants(
                            tenant_id UUID PRIMARY KEY,
-                           company_name VARCHAR(255) NOT NULL,
+                           company_name VARCHAR(255) UNIQUE NOT NULL,
                            plan VARCHAR(255) DEFAULT 'free',
                            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
                             """)
