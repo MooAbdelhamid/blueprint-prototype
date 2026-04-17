ca1508915270f8bbb84ba4f4560a82d965cfcc80
MooAbdelhamid <moethedataanalyst@gmail.com>
2026-04-17 20:01:36 +0200

    Removed email from tenants table

--- database/schemas/tenants.py
+++ database/schemas/tenants.py
@@ -3,7 +3,6 @@ def create_tenants_table(cursor):
                 CREATE TABLE IF NOT EXISTS tenants(
                            tenant_id UUID PRIMARY KEY,
                            company_name VARCHAR(255) NOT NULL,
-                           email VARCHAR(255) UNIQUE NOT NULL,
                            plan VARCHAR(255) DEFAULT 'free',
                            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
                             """)
